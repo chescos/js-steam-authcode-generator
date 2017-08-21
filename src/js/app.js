e6caa8d1832b2f4cffb81724807df15ea2abf751
Anon <yanik80@gmx.de>
2017-08-21 16:46:40 +0200

    added checking for invalid keys

--- src/js/app.js
+++ src/js/app.js
@@ -8,16 +8,25 @@
     DOM.result = $("#result");
 
     function init() {
-        // Events
+        // events
         DOM.generate.addEventListener("click", generateCode);
     }
 
     function generateCode() {
-        // Clear old generated
+        // clear old result
         DOM.result.innerHTML = "";
-        // Get the input values
+
+        // get the input secret
         var secret = DOM.secret.value;
-        var code = generateSteamGuardCode(secret);
+        var code = '';
+
+        // secret key is too short or does not end with "="
+        if(secret.length > 10 || secret.substr(secret.length - 1) !== '=') {
+            code = 'Invalid secret key';
+        }
+        else {
+            code = generateSteamGuardCode(secret);
+        }
 
         DOM.result.innerHTML = code;
     }
